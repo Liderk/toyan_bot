f5db6a02c9d425a0cd8c9506f979f931850ed8f1
lid <vismoke@yandex.ru>
2025-02-24 20:31:55 +0700

    исправление скрипта ожидания postgres

--- dockerization/scripts/wait_postgres.sh
+++ dockerization/scripts/wait_postgres.sh
@@ -8,10 +8,17 @@ postgres_ready() {
 python << END
 import sys
 import asyncio
+import os
 from psycopg import AsyncConnection
 
 async def connect_to_db():
     try:
+        POSTGRES_USER = os.getenv('POSTGRES_USER')
+        POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+        POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+        POSTGRES_PORT = os.getenv('POSTGRES_PORT', default='5432')
+        POSTGRES_DB = os.getenv('POSTGRES_DB')
+
         # Формируем DSN из переменных окружения
         dsn = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
