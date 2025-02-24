53e8e21b2c469ce0281d934a3bde0e25d2558226
lid <vismoke@yandex.ru>
2025-02-24 21:28:23 +0700

    изменение staticfiles

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -129,10 +129,12 @@ DATABASES = {
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = Path('/var/www/static') / 'staticfiles'
+if not STATIC_ROOT.exists():
+    STATIC_ROOT.mkdir(parents=True, exist_ok=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [STATIC_ROOT]
+STATICFILES_DIRS = []
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
