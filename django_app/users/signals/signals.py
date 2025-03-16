cb30d5c419c2a9106f8319956e18be4a8eb6e410
lid <vismoke@yandex.ru>
2025-03-16 16:28:24 +0700

    Изменение уведоления при одобрении

--- django_app/users/signals/signals.py
+++ django_app/users/signals/signals.py
@@ -1,4 +1,3 @@
-import asyncio
 from datetime import datetime, timedelta
 
 import pytz
@@ -8,22 +7,21 @@ from django.dispatch import receiver
 
 from users.models import TelegramUser
 from users.signals.custom_signals import telegram_user_accepted
-from utils.telegram_bot import bot
+from telegram import Bot
 
 
 @receiver(signal=telegram_user_accepted, sender=TelegramUser)
 def user_accepted(sender: Model, instance: TelegramUser, **kwargs):
-    asyncio.run(async_user_accepted(instance))
-
-
-async def async_user_accepted(instance):
     link_expired = 1
+    bot = Bot(token=settings.BOT_TOKEN)
+    expire_date = datetime.now(tz=pytz.timezone('Asia/Novosibirsk')) + timedelta(days=link_expired)
+    invite_link = bot.create_chat_invite_link(
+        chat_id=settings.CHAT_ID,
+        member_limit=1,  # Ссылка действует на 1 человека
+        expire_date=expire_date
+    )
     msg = (f'Кандидатура одобрена, получен доступ до бота\n\n'
            'Так же открылся доступ в группу Тоян: {chat_link}\n'
            'Ссылка действительна в течении 1 суток.')
-    expire_date = datetime.now(tz=pytz.timezone('Asia/Novosibirsk')) + timedelta(days=link_expired)
-    chat_invite_link = await bot.create_chat_invite_link(chat_id=settings.CHAT_ID,
-                                                         expire_date=expire_date, member_limit=1)
-    msg = msg.format(chat_link=chat_invite_link.invite_link, days=link_expired)
-
-    await bot.send_message(chat_id=instance.telegram_id, text=msg)
+    msg = msg.format(chat_link=invite_link, days=link_expired)
+    bot.send_message(chat_id=instance.telegram_id, text=msg)
--- django_app/utils/telegram_bot.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Bot
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from django.conf import settings
-
-bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
