f1c180abce26670a7f183fe23e90a861fbd08c4c
lid <vismoke@yandex.ru>
2025-03-22 11:49:45 +0700

    исправление создания пригласительной ссылки на канал

--- django_app/users/signals/signals.py
+++ django_app/users/signals/signals.py
@@ -15,7 +15,7 @@ def user_accepted(sender: Model, instance: TelegramUser, **kwargs):
     link_expired = 1
     bot = Bot(token=settings.BOT_TOKEN)
     expire_date = datetime.now(tz=pytz.timezone('Asia/Novosibirsk')) + timedelta(days=link_expired)
-    invite_link = bot.create_chat_invite_link(
+    link_obj = bot.create_chat_invite_link(
         chat_id=settings.CHAT_ID,
         member_limit=1,  # Ссылка действует на 1 человека
         expire_date=expire_date
@@ -23,5 +23,5 @@ def user_accepted(sender: Model, instance: TelegramUser, **kwargs):
     msg = (f'Кандидатура одобрена, получен доступ до бота\n\n'
            'Так же открылся доступ в группу Тоян: {chat_link}\n'
            'Ссылка действительна в течении 1 суток.')
-    msg = msg.format(chat_link=invite_link, days=link_expired)
+    msg = msg.format(chat_link=link_obj.invite_link, days=link_expired)
     bot.send_message(chat_id=instance.telegram_id, text=msg)
